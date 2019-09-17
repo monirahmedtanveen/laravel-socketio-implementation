3be23924e2454d004948bc9165ed07c7f7a633fd
monirahmedtanveen <monirahmedtanveen@gmail.com>
2019-09-17 10:20:24 +0600

    1. socket.js updated

--- socket.js
+++ socket.js
@@ -3,10 +3,10 @@ var io = require('socket.io')(server);
 var Redis = require('ioredis');
 var redis = new Redis();
  
-//redis.subscribe('user-channel');
+//redis.subscribe('user-channel'); // single channel
 redis.psubscribe('*'); //multiple channels
  
-//redis.on('message', function(channel, message) {
+//redis.on('message', function(channel, message) { // single channel
 redis.on('pmessage', function(subscribed, channel, message) { //multiple channels
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
